6a64cd57bba0dde9cdc32535be29e902886430b2
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2021-07-01 21:59:10 -0700

    optimize start of mock server

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,27 +23,20 @@ const defaultState = {
   currentListId: 7,
 };
 
-let server;
-// export const server = setupServer(...handlers);
-
+const server = setupServer(...handlers(defaultState));
+//
 // Establish API mocking before all tests.
-// beforeAll(() => server.listen())
+beforeAll(() => server.listen());
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
-// afterEach(() => server.resetHandlers())
+afterEach(() => server.resetHandlers());
 
 // Clean up after the tests are finished.
-// afterAll(() => server.close())
+afterAll(() => server.close());
 
 beforeEach(() => {
-  server = setupServer(...handlers(defaultState));
-  server.listen();
   render(App(defaultState));
-});
-
-afterEach(() => {
-  server.close();
 });
 
 describe('happy path', () => {
