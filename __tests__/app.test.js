95424bbcb9f51f926b1a30b1d0a211ff326c6ea1
Anatolii Burtsev <aburtsev@amazon.com>
2021-07-02 23:16:25 -0700

    add negative cases and general list flow test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,5 @@
 import {
-  render, screen, waitFor, waitForElementToBeRemoved,
+  act, render, screen, waitFor, waitForElementToBeRemoved,
 } from '@testing-library/react';
 import App from '@hexlet/react-todo-app-with-backend';
 import '@testing-library/jest-dom/extend-expect';
@@ -11,16 +11,15 @@ import handlers from '../handlers';
 const defaultState = {
   lists: [
     { id: 7, name: 'primary', removable: false },
-    { id: 8, name: 'secondary', removable: true },
   ],
   tasks: [
-    {
-      text: 'secondary task',
-      id: 8,
-      listId: 8,
-      completed: false,
-      touched: Date.now(),
-    },
+    // {
+    //   text: 'secondary task',
+    //   id: 8,
+    //   listId: 8,
+    //   completed: false,
+    //   touched: Date.now(),
+    // },
   ],
   currentListId: 7,
 };
@@ -48,33 +47,48 @@ class TodoListPage {
   }
 
   async addTask(text) {
-    userEvent.type(await this.screen.findByPlaceholderText('Please type text...'), text);
-    userEvent.click(await this.screen.findByRole('button', { name: 'Add' }));
+    if (text) userEvent.type(await this.screen.findByPlaceholderText('Please type text...'), text);
+    const button = await this.screen.findByRole('button', { name: 'Add' });
+    act(() => {
+      userEvent.click(button);
+    });
   }
 
-  async deleteTheOnlyTask() {
-    const removeButton = await this.screen.getByRole('button', { name: 'Remove' });
-    userEvent.click(removeButton);
+  async deleteTask(taskText) {
+    const removeButton = (await this.screen.findByText(taskText))
+      .parentElement.parentElement.parentElement
+      .querySelector('button.btn-danger');
+    act(() => {
+      userEvent.click(removeButton);
+    });
   }
 
-  getCheckboxElement() {
-    return this.screen.getByRole('checkbox');
+  async getCheckboxElement(taskText) {
+    return (await this.screen.findByText(taskText))
+      .parentElement.querySelector("input[type='checkbox']");
   }
 
-  toggleTheOnlyTask() {
-    const checkbox = this.getCheckboxElement();
-    userEvent.click(checkbox);
-  }
+  // async toggleTask(taskText) {
+  //   const checkbox = (await this.screen.findByText(taskText)).parentElement
+  //       .querySelector("label.pointer");
+  //   act(() => {
+  //     userEvent.click(checkbox);
+  //   });
+  // }
 
   async addList(name) {
     const addListButton = this.screen.getByTestId('list-form').querySelector('button[type="submit"]');
-    userEvent.type(await this.screen.findByPlaceholderText('List name...'), name);
-    userEvent.click(addListButton);
+    if (name) userEvent.type(await this.screen.findByPlaceholderText('List name...'), name);
+    act(() => {
+      userEvent.click(addListButton);
+    });
   }
 
   removeList(name) {
     const removeBtn = this.screen.getByText(name).parentElement.querySelector('button.link-danger');
-    userEvent.click(removeBtn);
+    act(() => {
+      userEvent.click(removeBtn);
+    });
   }
 }
 
@@ -92,43 +106,85 @@ describe('basic positive scenarios', () => {
   });
 
   test('general task flow', async () => {
-    const text = 'new task';
-    await todoListPage.addTask(text);
-    expect(await screen.findByText(text)).toBeInTheDocument();
+    const taskText = 'new task';
+    await todoListPage.addTask(taskText);
+    expect(await screen.findByText(taskText)).toBeInTheDocument();
 
-    todoListPage.toggleTheOnlyTask();
-    const checkbox = todoListPage.getCheckboxElement();
+    const checkbox = await todoListPage.getCheckboxElement(taskText);
+
+    userEvent.click(checkbox);
     await waitFor(() => expect(checkbox).toBeChecked());
+    userEvent.click(checkbox);
+    await waitFor(() => expect(checkbox).not.toBeChecked());
 
-    await todoListPage.deleteTheOnlyTask();
+    await todoListPage.deleteTask(taskText);
     expect(await todoListPage.emptyCurrentListSelector()).toBeInTheDocument();
   });
 
   test('general list flow', async () => {
-    await todoListPage.addTask('task in the old list');
+    const oldListTaskText = 'task in the old list';
+    await todoListPage.addTask(oldListTaskText);
+    expect(await screen.findByText(oldListTaskText)).toBeVisible();
 
     const listName = 'newlist';
     await todoListPage.addList(listName);
     expect(await screen.findByText(listName)).toBeInTheDocument();
     expect(await todoListPage.emptyCurrentListSelector()).toBeInTheDocument();
+    expect(screen.queryByText(oldListTaskText)).not.toBeInTheDocument();
 
     const taskText = 'new task in the new list';
     await todoListPage.addTask(taskText);
+    expect(await screen.findByText(taskText)).toBeInTheDocument();
 
     await todoListPage.removeList(listName);
     await waitForElementToBeRemoved(() => screen.getByText(listName));
-    // await waitForElementToBeRemoved(() => screen.getByText(taskText));
-    // expect(screen.getByText(listName)).not.toBeInTheDocument();
-    // expect(screen.getByText(taskText)).not.toBeInTheDocument();
+    // previous list doesn't become active in test, but does in browser
+    userEvent.click(screen.getByText('primary'));
+    expect(await screen.findByText(oldListTaskText)).toBeInTheDocument();
+    expect(screen.getByText('primary')).toHaveClass('link-primary');
   });
 });
 
 describe('negative scenarios', () => {
-  test('api error', async () => {
+  test('task api error', async () => {
     server.use(
       rest.post('/api/v1/lists/:id/tasks', (req, res, ctx) => res(ctx.status(500))),
     );
     await todoListPage.addTask('new task');
     expect(await screen.findByText('Network error')).toBeInTheDocument();
+  });
+
+  test('list api error', async () => {
+    server.use(
+      rest.post('/api/v1/lists', (req, res, ctx) => res(ctx.status(500))),
+    );
+    await todoListPage.addList('new list');
+    expect(await screen.findByText('Network error')).toBeInTheDocument();
+  });
+
+  test('task duplicate', async () => {
+    const taskText = 'new task text';
+    await todoListPage.addTask(taskText);
+    expect(await screen.findByText(taskText)).toBeInTheDocument();
+    await todoListPage.addTask(taskText);
+    expect(await screen.findByText(`${taskText} already exists`)).toBeInTheDocument();
+  });
+
+  test('list duplicate', async () => {
+    const listName = 'new list name';
+    await todoListPage.addList(listName);
+    expect(await screen.findByText(listName)).toBeInTheDocument();
+    await todoListPage.addList(listName);
+    expect(await screen.findByText(`${listName} already exists`)).toBeInTheDocument();
+  });
+
+  test('empty task text', async () => {
+    await todoListPage.addTask('');
+    expect(await screen.findByText('Required!')).toBeInTheDocument();
+  });
+
+  test('empty list name', async () => {
+    await todoListPage.addList('');
+    expect(await screen.findByText('Required!')).toBeInTheDocument();
   });
 });
