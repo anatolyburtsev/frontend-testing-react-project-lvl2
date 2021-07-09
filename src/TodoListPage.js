832aeafacfd0efd57ebe35a6a220415bff8431f4
Anatolii Burtsev <aburtsev@amazon.com>
2021-07-08 20:14:18 -0700

    remove act

--- src/TodoListPage.js
+++ src/TodoListPage.js
@@ -1,5 +1,4 @@
 import userEvent from '@testing-library/user-event';
-import { act } from '@testing-library/react';
 
 class TodoListPage {
   constructor(appScreen) {
@@ -13,18 +12,14 @@ class TodoListPage {
   async addTask(text) {
     if (text) userEvent.type(await this.screen.findByPlaceholderText('Please type text...'), text);
     const button = await this.screen.findByRole('button', { name: 'Add' });
-    act(() => {
-      userEvent.click(button);
-    });
+    userEvent.click(button);
   }
 
   async deleteTask(taskText) {
     const removeButton = (await this.screen.findByText(taskText))
       .parentElement.parentElement.parentElement
       .querySelector('button.btn-danger');
-    act(() => {
-      userEvent.click(removeButton);
-    });
+    userEvent.click(removeButton);
   }
 
   async getCheckboxElement(taskText) {
@@ -35,16 +30,12 @@ class TodoListPage {
   async addList(name) {
     const addListButton = this.screen.getByTestId('list-form').querySelector('button[type="submit"]');
     if (name) userEvent.type(await this.screen.findByPlaceholderText('List name...'), name);
-    act(() => {
-      userEvent.click(addListButton);
-    });
+    userEvent.click(addListButton);
   }
 
   removeList(name) {
     const removeBtn = this.screen.getByText(name).parentElement.querySelector('button.link-danger');
-    act(() => {
-      userEvent.click(removeBtn);
-    });
+    userEvent.click(removeBtn);
   }
 }
 
